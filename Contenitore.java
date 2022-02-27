7c002fba923b10e5ca4eb9697147bd65600fbd02
andreramolivaz <92636448+andreramolivaz@users.noreply.github.com>
2022-02-27 17:37:16 +0100

    Add files via upload

---
+++ Contenitore.java
@@ -0,0 +1,45 @@
+package cofruit;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Contenitore {
+    private final static int dimensioneContenitore = 3; // dimensione del contenitore
+    
+    private ArrayList <Mela> mele;
+
+    public Contenitore() {
+        mele= new ArrayList(dimensioneContenitore); //definisco arraList
+    }
+    
+    public synchronized void inserisci(Mela mela)
+    {
+        while(mele.size()== dimensioneContenitore){
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Contenitore.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        mele.add(mela);
+        notifyAll();
+    }
+    
+    public synchronized Mela estrai(){
+        while(mele.size()==0) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Contenitore.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        Mela mela=mele.get(0);
+        mele.remove(0);
+        notifyAll();
+        
+        return mela;
+    }
+}
---
+++ Produttore.java
@@ -0,0 +1,39 @@
+package cofruit;
+
+import java.util.Random;
+
+
+public class Produttore implements Runnable {
+    private Contenitore contenitoreMele;
+
+    public Produttore(Contenitore contenitoreMele) {
+        this.contenitoreMele = contenitoreMele;
+    }
+    
+    @Override
+    public void run(){
+        Mela mela;
+        Random nRandom = new Random();
+        int estratto, marcia;
+        
+        for(int i=0; i<15; i++) {
+            estratto = (int) nRandom.nextInt(2); //Estraggo il numero per vedere se gialla/Rossa
+            marcia = (int) nRandom.nextInt(2); //Estraggo il numero per vedere se e' marcia o meno
+            
+            if(estratto==1) { //Controllo se mela e' rossa
+                mela = new Mela(i, "Rossa", true);
+            }
+            else {
+                mela = new Mela(i, "Gialla", true); //altrimenti e' gialla
+            }
+            
+            if(marcia==1) { //Controllo se e' marcia (1) imposto setStatoMela() a true, altrimenti a false
+                mela.setStatoMela(true);
+            }
+            else {
+                mela.setStatoMela(false);
+            }
+            contenitoreMele.inserisci(mela); //Inserisco la mela nel contenitore
+        }
+    }
+}
